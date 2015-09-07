c3769d9425b5ba2d2f9e2c40ad8dcb38a073f50c
Roma Kuzmin <romik-off-road@yandex.ru>
2015-09-07 16:28:53 +0300

    i123

--- ESP8266_ARDUINO_TEST.ino
+++ ESP8266_ARDUINO_TEST.ino
@@ -1,29 +1,81 @@
+// Arduino 1.0.6
 
+// #define _SS_MAX_RX_BUFF 64 // RX buffer size => 256
+// SoftwareSerial.h Изменить размер RX буфера.
+
+// ESP 8266 Version
+
+// AT version:0.22.0.0(Mar 20 2015 10:04:26)
+// SDK version:1.0.0
+// compile time:Mar 20 2015 11:00:32
+
+//  AT+CWSAP_DEF="GPS","",6,0
+//  AT+CWSAP_CUR="GPS","",6,0
+
+//  AT+CWMODE=3  Soft_AP + Client
+// AT+CIPMUX=1 Много соединений
+// AT+CIPSERVER=1,80  Сервер на 80 порту
+
+// https://github.com/itead/ITEADLIB_Arduino_WeeESP8266
+// 
+// #define USER_SEL_VERSION	VERSION_22
+// #define ESP8266_USE_SOFTWARE_SERIAL
+
+#include "ESP8266.h"
 #include <SoftwareSerial.h>
+ 
+ SoftwareSerial mySerial(2,3); // RX, TX
+ 
+#define SSID                "OS"
+#define PASSWORD    "4957392716"
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+ESP8266 wifi(mySerial);
 
-void setup()  
+uint8_t buffer[128] = {0};
+uint8_t mux_id;
+uint32_t len = 0;
+
+char s[128];
+
+void setup(void)
 {
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 
+  pinMode(A0,OUTPUT);
+  digitalWrite(A0,LOW);
+  
+   Serial.begin(57600);
+   mySerial.begin(57600);
+    
+    wifi.setOprToStationSoftAP();
 
-  Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(115200);
-  mySerial.println("Hello, world?");
-}
-
-void loop() // run over and over
+    if (wifi.joinAP(SSID, PASSWORD)) {
+       strcpy(s,wifi.getLocalIP().c_str());
+    } else {
+       strcpy(s,"No IP");
+    }
+    
+    wifi.enableMUX();
+    wifi.startTCPServer(80);
+    wifi.setTCPServerTimeout(10);
+ }
+ 
+ 
+void loop(void)
 {
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
-}
+    
+   digitalWrite(A0,HIGH);
 
+    if (mySerial.available()) {
+      len = wifi.recv(&mux_id, buffer, sizeof(buffer), 100);
+    }
+    
+   digitalWrite(A0,LOW);
+
+    if (len > 0) {
+      wifi.send(mux_id, (const uint8_t*)s, strlen(s) );
+      wifi.releaseTCP(mux_id);
+      len = 0;
+    }
+
+}
+        
